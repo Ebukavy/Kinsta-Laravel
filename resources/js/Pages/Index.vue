ec50dd867e31fa6725f6e27578a1dc2b7e826fc6
Gideon Tombe <133422903+Ebukavy@users.noreply.github.com>
2024-06-16 03:01:07 +0200

    s

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import KinstaLayout from "../Layouts/KinstaLayout.vue";
+  import { Link } from "@inertiajs/inertia-vue3";
+
+  defineProps({
+    articles: Object,
+  });
+</script>
+
+<template>
+  <header>
+    <h1>Kinsta Blog</h1>
+  </header>
+
+  <main>
+    <h2>Read our latest articles</h2>
+
+    <section>
+      // Looping over articles
+      <article v-for="article in articles":key="article.id">
+        <div>
+          <img src="/images/kinsta-logo.png" alt="Article thumbnail" />
+        </div>
+
+        <h3>{{article.title}}</h3>
+        <p>{{article.excerpt}}</p>
+
+        <Link
+            :href="'/posts/' + article.id"
+            class="text-indigo-600 hover:text-indigo-800 w-fit self-end font-semibold"
+            >Read more
+        </Link>
+
+      </article>
+    </section>      
+  </main>
+
+  <footer>
+    <h2>Join our Newsletter</h2>
+
+    <input type="email" />
+  </footer>
+</template>
